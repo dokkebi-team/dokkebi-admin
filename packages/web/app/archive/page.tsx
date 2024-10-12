5b0f9d5cf8c7f02ae91e3583c509e1a01f65db66
jaeseokk <kang89kr@gmail.com>
2024-10-12 16:30:12 +0900

    Add archive page

---
+++ packages/web/app/archive/page.tsx
@@ -0,0 +1,11 @@
+import Test from "@/components/Test";
+
+export interface ArchivePageProps {
+  children: React.ReactNode;
+}
+
+const ArchivePage = ({ children }: ArchivePageProps) => {
+  return <Test />;
+};
+
+export default ArchivePage;
