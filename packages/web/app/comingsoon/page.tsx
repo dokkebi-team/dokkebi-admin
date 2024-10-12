5ddde54542d98b3208119a6fb787806bbc8c814c
jaeseokk <kang89kr@gmail.com>
2024-10-12 17:30:31 +0900

    Add commingsoon page

---
+++ packages/web/app/comingsoon/page.tsx
@@ -0,0 +1,11 @@
+export interface ComingsoonPageProps {}
+
+const ComingsoonPage = ({}: ComingsoonPageProps) => {
+  return (
+    <div className="h-screen-ios min-h-screen-ios flex h-screen min-h-screen items-center justify-center">
+      <h1 className="font-hail text-2xl text-[#67FF5B]">coming soon</h1>
+    </div>
+  );
+};
+
+export default ComingsoonPage;
