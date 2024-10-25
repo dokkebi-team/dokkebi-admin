96c5d708308b319a66a662934e8d060e7c86455b
jaeseokk <kang89kr@gmail.com>
2024-10-25 09:33:14 +0900

    Randomize initial position

--- packages/web/stores/map.ts
+++ packages/web/stores/map.ts
@@ -1,15 +1,18 @@
 import { atom } from "jotai";
 import { selectAtom } from "jotai/utils";
 import { PlayerState } from "../components/Player";
-import { INITIAL_POSITION } from "../constants";
+import { INITIAL_POSITIONS } from "../constants";
 
 export const isAppStartedAtom = atom(false);
+
+const randomIndex = Math.floor(Math.random() * INITIAL_POSITIONS.length);
+const initialPosition = INITIAL_POSITIONS[randomIndex];
 
 export const playerAtom = atom<{
   position: { x: number; y: number };
   state: PlayerState;
 }>({
-  position: { x: INITIAL_POSITION.x, y: INITIAL_POSITION.y },
+  position: initialPosition,
   state: {
     animationStatus: "idle",
     direction: "down",
